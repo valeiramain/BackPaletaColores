ea4519e6e6f83eaca70a9f066acf1b40e864db3e
valeiramain <valentinairamain2020@gmail.com>
2026-01-18 18:02:47 -0300

    creamos carpeta middlewares para validaciones, y los archivos validacionColor y resultadoValidacion

---
+++ src/middlewares/validacionColor.js
@@ -0,0 +1,27 @@
+import { body } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion.js";
+import Color from "../models/Color.js";
+
+const validacionColor = [
+  body("nombre")
+    .notEmpty()
+    .withMessage("El nombre del color es un dato obligatorio.")
+    .isLength({ min: 3, max: 30 })
+    .withMessage("El nombre del color debe tener entre 3 y 30 caracteres.")
+    .isString()
+    .withMessage("El nombre del color debe ser un texto.")
+    .custom(async (valor,{req}) => {
+      const colorExistente = await Color.findOne({ nombre: valor });      
+      if (!colorExistente) {        
+        return true; // el color no existe, la validación es exitosa
+      } 
+      if (req.params?.id && colorExistente._id.toString() === req.params.id) {
+        return true; // el color existe pero es el mismo que estamos actualizando
+      } 
+    throw new Error("El nombre del color ya está registrado en la base de datos.");
+    }),
+     
+    (req, res, next) => resultadoValidacion(req, res, next)
+]
+
+export default validacionColor;
